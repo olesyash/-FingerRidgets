4f3db69dad36ff85c1d1dddd2150dfd168efd14c
olesya <ilana.olesya@gmail.com>
2024-04-19 17:50:52 +0300

    Compare all steps

--- abc_finger_region_recognizer.py
+++ abc_finger_region_recognizer.py
@@ -21,48 +21,48 @@ class FingerRegionRecognizer(ABC):
     def calculate_ridge_frequencies(self) -> str:
         pass
 
-    @abstractmethod
-    def calculate_block_frequencies(self) -> str:
-        pass
+    # @abstractmethod
+    # def calculate_block_frequencies(self) -> str:
+    #     pass
 
     @abstractmethod
     def apply_gabor_filter(self) -> str:
         pass
 
-    @abstractmethod
-    def skeletonize(self) -> str:
-        pass
+    # @abstractmethod
+    # def skeletonize(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def detect_minutiae(self) -> str:
-        pass
+    # @abstractmethod
+    # def detect_minutiae(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def calculate_minutiae_weights(self) -> str:
-        pass
+    # @abstractmethod
+    # def calculate_minutiae_weights(self) -> str:
+    #     pass
 
-    @abstractmethod 
-    def count_lines(self) -> str:
-        pass
+    # @abstractmethod 
+    # def count_lines(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def count_diagonal_lines(self) -> str:
-        pass
+    # @abstractmethod
+    # def count_diagonal_lines(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def draw_diagonal(self) -> str:
-        pass
+    # @abstractmethod
+    # def draw_diagonal(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def get_best_region(self) -> str:
-        pass
+    # @abstractmethod
+    # def get_best_region(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def draw_ridges_count_on_region(self) -> str:
-        pass
+    # @abstractmethod
+    # def draw_ridges_count_on_region(self) -> str:
+    #     pass
 
-    @abstractmethod
-    def count_fingerprint_ridges(self) -> str:
-        pass
+    # @abstractmethod
+    # def count_fingerprint_ridges(self) -> str:
+    #     pass
 
 
